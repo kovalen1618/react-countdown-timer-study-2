f48dfc749c72508a7530fd6af06f656b4f7893f4
kovalen1618 <kovalen1.618@gmail.com>
2023-07-17 16:49:06 -0500

    Created CountdownTimer component

---
+++ src/components/CountdownTimer.jsx
@@ -0,0 +1,15 @@
+function CountdownTimer() {
+  return (
+    <div className="countdown-timer"> 
+        <span>30</span>
+        <span>days</span>
+        <span>12</span>
+        <span>hours</span>
+        <span>04</span>
+        <span>minutes</span>
+        <span>32</span>
+        <span>seconds</span>
+    </div>
+  )
+}
+export default CountdownTimer
